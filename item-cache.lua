cbdad552757b72ee90d9ec3391f0713363aa6b39
lexifuzzpup <lexifuzzpup@gmail.com>
2026-08-19 17:16:27 -0500

    Fix parallelism issue with item cache

--- item-cache.lua
+++ item-cache.lua
@@ -9,18 +9,29 @@ local ItemDetail = {}
 
 local item_cache = {}
 
+-- parallel-safe item cache
+function item_cache.get(name)
+    ---@type table
+    local item_detail
+
+    repeat
+        item_detail = item_cache[name]
+    until item_detail ~= true
+
+    return item_detail
+end
+
 ---@param name string namespaced id of the item
 ---@param inventory Inventory inventory the item is stored in
 ---@param slot_id integer slot the item is stored in
 function item_cache.add_if_not_present(name, inventory, slot_id)
-    local item_detail = item_cache[name]
-
-    if item_detail ~= nil then return end
+    if item_cache[name] ~= nil then return end
     if inventory.interface == nil then error("Inventory is not attached") end
 
+    item_cache[name] = true
     local slot_detail = inventory.interface.getItemDetail(slot_id)
 
-    item_detail = {
+    local item_detail = {
         name = name,
         stack_size = slot_detail.maxCount,
         display_name = slot_detail.displayName,
